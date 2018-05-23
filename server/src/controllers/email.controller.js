89333b8caa31c1908c67af561914cd2dbd84b545
czg <czg@jfpal.com>
2018-05-23 09:57:11 +0800

    提交更新

--- server/src/controllers/email.controller.js
+++ server/src/controllers/email.controller.js
@@ -4,13 +4,17 @@ class EmailController {
 
     // 发送邮件
     async send (ctx, next) {
+        console.log('进入控制器，即将验证参数')
         ctx.checkBody({
             email: {
                 optional: true,
                 isEmail: { errorMessage: 'email 格式不正确' },
             },
         });
-        if (ctx.validationErrors()) return null;
+        // if (ctx.validationErrors()) return null;
+        console.log(ctx.validationErrors())
+        console.log('验证参数完成')
+        return null;
         try {
             ctx.pipeDone();
         } catch (e) {
