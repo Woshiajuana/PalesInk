9871ca6170a8ea06277d9ca1208906d97429310c
czg <czg@jfpal.com>
2018-05-29 09:16:08 +0800

    更新

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -10,18 +10,20 @@ class EmailService {
 
     // 发送验证码
     async send (recipient) {
-        // 读取文件
-        fs.readFile(path.join(__dirname, '../views/email.html'), "utf-8", async (err, file) => {
-            if (err && err.code === 'ENOENT') throw '邮件模板 not find';
-            if (err) throw err;
-            try {
-                let check_code = wowCool.obtainRandomNumber(emailConfig.email.limit);
-                let content = file.replace(/{{}}/, check_code);
-                return await emailUtil.send(recipient, emailConfig.subject, content);
-            } catch (e) {
-                throw e;
-            }
+        return new Promise( async (resolve, reject) => {
+            fs.readFile(path.join(__dirname, '../views/email.html'), "utf-8", async (err, file) => {
+                try {
+                    if (err && err.code === 'ENOENT') throw('邮件模板 not find');
+                    if (err) throw err;
+                    let check_code = wowCool.obtainRandomNumber(emailConfig.email.limit);
+                    let content = file.replace(/{{}}/, check_code);
+                    return await emailUtil.send(recipient, emailConfig.subject, content);
+                } catch (err) {
+                    reject(err);
+                }
+            });
         });
+
     }
 
 }
