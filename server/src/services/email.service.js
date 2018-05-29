212ada4e99242b367c5321c89097170016026fb8
czg <czg@jfpal.com>
2018-05-29 13:56:28 +0800

    更新

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -18,7 +18,7 @@ class EmailService {
                     if (err) throw err;
                     let check_code = wowCool.obtainRandomNumber(emailConfig.email.limit);
                     let redis_client = await redisUtil.set(recipient, check_code);
-                    redis_client.expire(recipient, 30);
+                    redis_client.expire(recipient, 30000);
                     let content = file.replace(/{{}}/, check_code);
                     await emailUtil.send(recipient, emailConfig.subject, content);
                     return resolve();
@@ -32,7 +32,7 @@ class EmailService {
     // 验证验证码
     async check (recipient, check_code) {
         return new Promise( async (resolve, reject) => {
-            try { 
+            try {
                 let redis_code = await redisUtil.get(recipient);
                 console.log('取出来的code', redis_code);
                 redis_code === check_code ? resolve() : reject('验证失败')
