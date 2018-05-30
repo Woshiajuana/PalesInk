b73bcd458ef55eb7f1e428c6eca8d4c9e292d35c
czg <czg@jfpal.com>
2018-05-30 11:40:35 +0800

    优化代码

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,5 @@
-import UserModel                        from './../models/user.model'
+
 import UserService                      from './../services/user.service'
-import EmailService                     from './../services/email.service'
 
 class UserController {
 
